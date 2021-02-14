c0cfff45544aae96a933a3e875b7627721b3f740
Vladyslav-Kondrenko <nesudba2@gmail.com>
2021-02-14 23:09:38 +0200

    add function writeYourGenres and showMyDB

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 /* Задание на урок:
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+1) Первую часть задания повторить по уроку
 
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
 
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
 
-4) Потренироваться и переписать цикл еще двумя способами*/
+P.S. Функции вызывать не обязательно*/
 
 'use strict';
 
@@ -30,7 +29,10 @@ let personalMovieDB = {
       movieRating;
   
 // 1-2
-  for(let i = 0; i < 2; i++){
+// personalMovies();
+
+function personalMovies(){
+    for( let i = 0; i < 2; i++ ){
     movieName = prompt('Один из последних просмотренных фильмов?', '');
     movieRating = prompt('На сколько оцените его?', '');
     if( movieName == '' || movieName.length > 50 || movieName == null || movieRating == '' || movieRating == null){
@@ -40,9 +42,13 @@ let personalMovieDB = {
     }
     personalMovieDB.movies[movieName] = movieRating;
   }
+}
 
   // 3
-if(Number.isInteger(personalMovieDB.count)){
+// checkCountOfMovies();
+
+function checkCountOfMovies(){
+  if(Number.isInteger(personalMovieDB.count)){
 
      if(personalMovieDB.count < 10){
      alert('Просмотрено довольно мало фильмов');
@@ -52,8 +58,33 @@ if(Number.isInteger(personalMovieDB.count)){
         alert('Вы киноман');
     }
 
-} else{
-    alert('Произошла ошибка!');
+  } else{
+      alert('Произошла ошибка!');
+  }
 }
 
-  console.log('personalMovieDB', personalMovieDB);
+
+writeYourGenres();
+
+function writeYourGenres(){
+  for( let i = 1; i <= 3; i++ ){
+    let genres = prompt(`Ваш любимый жанр под номером ${i}`);
+    if( genres == '' || genres == null){
+      alert('Ошибка. Попробуйте снова.')
+      i--;
+    } else{
+      console.log(i);
+      personalMovieDB.genres[i - 1] = genres; 
+    }
+  }
+}
+
+  showMyDB();
+
+function showMyDB(){
+  if(!personalMovieDB.privat){
+    console.log(personalMovieDB);
+  }
+}
+
+  
